90ae217d1153c221e1063ec2a3e04984e21356cb
Rafiki <58946529+RanoZangana@users.noreply.github.com>
2022-02-11 15:18:00 +0100

    Mina ändringar

--- Image_Search_GroupC/image_search.js
+++ Image_Search_GroupC/image_search.js
@@ -3,11 +3,25 @@ start()
 async function start() {
 
     let param = new URLSearchParams({
-        key: '25645717-03cdc3424e75e0888d9a391cd',
-        q: 'dog puppy'
+        key: '25592393-ccf7ee53d7b027ae015f7e308',
+        per_page: 10,
+        q: 'dog'
     })
-    let response = await fetch('https://pixabay.com/api/?' +
-        param.toString());
-    let json = await response.json;
+    const myApi = 'https://pixabay.com/api/?' + param.toString();
+    let response = await fetch(myApi);
+    let jsonData = await response.json();
+
+    let resultImageList = document.querySelector('#results-image-list')
+    let imageTemplate = document.querySelector('#results-template');
+    imageTemplate.remove();
+
+    for (let image of jsonData.hits) {
+        let li = imageTemplate.content.firstElementChild.cloneNode(true);
+        li.querySelector('.results-image').src = image.largeImageURL;
+        li.querySelector('.results-image-tag').textContent = image.tags;
+        li.querySelector('.results-image-user').textContent = image.user;
+        resultImageList.append(li);
+    }
+
 
 }
